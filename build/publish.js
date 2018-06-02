57a3abc9666a13b245afc2d74232b7243c9c1622
Andy Tran <andy8tran@gmail.com>
2018-06-02 15:57:28 -0400

    Add command for pushing to gh pages

---
+++ build/publish.js
@@ -0,0 +1,8 @@
+const ghpages = require('gh-pages');
+const path = require('path');
+
+ghpages.publish(path.join(__dirname, '../dist'), err => {
+  console.log('Error publishing to gh-pages branch...');
+  console.log(path.join(__dirname, '../dist'));
+  console.log(err);
+});
