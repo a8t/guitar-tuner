e41389a4e71d4c44c6dff497ba4c6b96504c54ea
Andy Tran <andy8tran@gmail.com>
2018-06-02 12:27:36 -0400

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import NeedleTuner from '@/components/NeedleTuner';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'NeedleTuner',
+      component: NeedleTuner,
+    },
+  ],
+});
