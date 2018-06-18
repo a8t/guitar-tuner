b81ae57bcd7ef7d4d60425ccf99729e24c615ddb
Andy Tran <andy8tran@gmail.com>
2018-06-18 16:11:29 -0400

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import TunerPage from '@/pages/TunerPage'
+import NeedleTuner from '@/components/Tuner/NeedleTuner/NeedleTuner'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      redirect: 'tuner',
-    },
-    {
       path: '/tuner',
       name: 'Tuner Page',
       component: TunerPage,
+      children: [
+        {
+          path: '',
+          redirect: 'needle-tuner',
+        },
+        {
+          path: 'needle-tuner',
+          component: NeedleTuner,
+        },
+        {
+          path: '*',
+          redirect: 'needle-tuner',
+        },
+      ],
+    },
+    {
+      path: '*',
+      redirect: 'tuner',
     },
   ],
 })
