df67871b1885609d2d9535daf22944d84bfc1fbf
Andy Tran <andy8tran@gmail.com>
2018-06-19 12:54:09 -0400

    Remove fluff from TunerProvider

--- src/components/Tuner/TunerProvider.vue
+++ src/components/Tuner/TunerProvider.vue
@@ -5,21 +5,9 @@
 
 <script>
 import tunerMixin from '@/components/mixins/tunerMixin'
-import NeedleTuner from '@/components/Tuner/NeedleTuner/NeedleTuner'
-import NeedleDisplay from '@/components/Tuner/NeedleTuner/NeedleDisplay'
 
 export default {
   name: 'TunerProvider',
-  components: {
-    NeedleTuner,
-    NeedleDisplay,
-  },
   mixins: [tunerMixin],
 }
 </script>
-
-<style>
-NeedleDisplay {
-  display: none;
-}
-</style>
